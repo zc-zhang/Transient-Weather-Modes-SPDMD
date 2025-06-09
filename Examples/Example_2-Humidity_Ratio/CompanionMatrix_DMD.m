b723401ba66e481c8da93fe4e5de9336a39a3065
Zhicheng (Charlie) Zhang <59109586+zc-zhang@users.noreply.github.com>
2025-06-10 08:38:45 +0900

    Update CompanionMatrix_DMD.m

--- Examples/Example_2-Humidity_Ratio/CompanionMatrix_DMD.m
+++ Examples/Example_2-Humidity_Ratio/CompanionMatrix_DMD.m
@@ -1,8 +1,11 @@
-function [ KModes,KEv,Norms,XCdmd ] = CompanionMatrix_DMD( Data )
+function [ KModes,KEv,KAmps,Norms,XCdmd ] = CompanionMatrix_DMD( Data )
+
+% References:
 % Dynamic Mode Decomposition as presented by 
 % "Spectral analysis of nonlinear flows" by Rowley et al., Journal of FLuid
 % Mechanics, 2009
-
+% "Ergodic theory, dynamic mode decomposition, and computation of spectral properties of the Koopman operator"
+%  by Hassan Arbabi and Ignor Mezic, SIADS'17
 
 % inputs : 
 % Data - the data matrix: each column is a a set of measurements done at
@@ -17,6 +20,8 @@ function [ KModes,KEv,Norms,XCdmd ] = CompanionMatrix_DMD( Data )
 % that make up the time-varaiation of data in time
 
 % 3- Norms - Euclidean (vector) norm of each mode, used to sort the data
+
+% 4- XCdmd - Reconstruction or superposition of the Companion DMD or KMD.
 
 
 
@@ -36,26 +41,27 @@ KMunsrtd = X*P;                         %% Koopman Modes
 
  Cnorm = sqrt(sum(abs(KMunsrtd).^2,1));  %% Euclidean norm of Koopman Modes
  [Norms, ind]=sort(Cnorm,'descend');      %% sorting based on the norm KM
-%[Norms, ind]=sort(real(KEunsrtd),'descend');          %% %% sorting based on ke
+ %[Norms, ind]=sort(real(KEunsrtd),'descend');          %% sorting based on real part of Koopman eigenvalues
  KEv= KEunsrtd(ind);           %% Koopman Eigenvalues
 
 KModes = KMunsrtd(:,ind);                %% Koopman Modes
+
+% Compute modal amplitudes (initial projection onto Koopman modes)
+ b = KModes\X(:,1);  % Least-squares solution 
+% b = pinv(KModes) * X(:,1);   % or consider (pseudo-inverse of KModes)
+ KAmps=b; % rename amplitudes
 
 % Time evolution (you can modify the time steps `t` as needed)
 t = 0:(size(Data,2)-2);  %% Time steps corresponding to the snapshots
 Vandermonde = KEv(:).^t;  %% Vandermonde matrix for time evolution
 
 % Reconstruct the data using the Koopman modes and time evolution
-XCdmd = KModes * Vandermonde;  %% Reconstructed data
-
-
-
+XCdmd = KModes * (Vandermonde .* b);  % Element-wise multiply by amplitudes
+%XCdmd = KModes * diag(b)*Vandermonde;  % Element-wise multiply by amplitudes 
 end
 
 
-%=========================================================================%
-% Hassan Arbabi - 08-17-2015
-% Mezic research group
+
 % UC Santa Barbara
 % arbabiha@gmail.com
 %=========================================================================%
